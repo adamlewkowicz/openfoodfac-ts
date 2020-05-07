c6755be9bd7cd36653df123f8b50623858194403
alk831 <adam54111@gmail.com>
2020-05-07 12:29:40 +0200

    finish api mock

--- mock/index.ts
+++ mock/index.ts
@@ -3,13 +3,34 @@ import productResponse from '../src/OpenFoodFactsApi/__mocks__/product-response.
 import productsResponse from '../src/OpenFoodFactsApi/__mocks__/products-response.json';
 import tagsResponse from '../src/OpenFoodFactsApi/__mocks__/tags-response.json';
 
-const getProductResponse = async () => productResponse;
-const getProductsResponse = async () => productsResponse;
-const getTagsResponse = async () => tagsResponse;
+const getProductResponse = () => jest.fn(async () => productResponse) as any;
+const getProductsResponse = () => jest.fn(async () => productsResponse) as any;
+const getTagsResponse = () => jest.fn(async () => tagsResponse);
 
 // @ts-ignore
 export class OpenFoodFactsApi implements NativeOpenFoodFactsApi {
 
-  findProductsBySearchTerm = getProductsResponse;
+  findProductByBarcode = getProductResponse();
+
+  findProductsBySearchTerm = getProductsResponse();
+  findProductsByBrand = getProductsResponse();
+  findProductsByCategory = getProductsResponse();
+
+  findAdditives = getTagsResponse();
+  findAllergens = getTagsResponse();
+  findBrands = getTagsResponse();
+  findCategories = getTagsResponse();
+  findCountries = getTagsResponse();
+  findEntryDates = getTagsResponse();
+  findIngredients = getTagsResponse();
+  findLanguages = getTagsResponse();
+  findPackagingCodes = getTagsResponse();
+  findPackagings = getTagsResponse();
+  findPurchasePlaces = getTagsResponse();
+  findStates = getTagsResponse();
+  findTraces = getTagsResponse();
+
+  setController = jest.fn();
+  setUserAgent = jest.fn();
 
 }
