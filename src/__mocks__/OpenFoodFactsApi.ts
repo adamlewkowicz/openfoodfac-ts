c9bffea3be329ead7de7a66c971ae445a4839860
alk831 <adam54111@gmail.com>
2020-05-07 12:32:22 +0200

    move mocks

--- src/__mocks__/OpenFoodFactsApi.ts
+++ src/__mocks__/OpenFoodFactsApi.ts
@@ -1,7 +1,7 @@
-import { OpenFoodFactsApi as NativeOpenFoodFactsApi } from '../src/OpenFoodFactsApi';
-import productResponse from '../src/OpenFoodFactsApi/__mocks__/product-response.json';
-import productsResponse from '../src/OpenFoodFactsApi/__mocks__/products-response.json';
-import tagsResponse from '../src/OpenFoodFactsApi/__mocks__/tags-response.json';
+import { OpenFoodFactsApi as NativeOpenFoodFactsApi } from '../OpenFoodFactsApi';
+import productResponse from '../OpenFoodFactsApi/__mocks__/product-response.json';
+import productsResponse from '../OpenFoodFactsApi/__mocks__/products-response.json';
+import tagsResponse from '../OpenFoodFactsApi/__mocks__/tags-response.json';
 
 const getProductResponse = () => jest.fn(async () => productResponse) as any;
 const getProductsResponse = () => jest.fn(async () => productsResponse) as any;
